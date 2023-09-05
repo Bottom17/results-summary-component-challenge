6419c0623c8c3288df4cb19f3b41168cfacf5041
Bottom17 <101359677+Bottom17@users.noreply.github.com>
2023-09-05 17:19:12 +0100

    Results Summary Component Solution

---
+++ README.md
@@ -0,0 +1,76 @@
+# Frontend Mentor - Results summary component solution
+
+This is a solution to the [Results summary component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+- **Bonus**: Use the local JSON data to dynamically populate the content
+
+### Screenshot
+
+<h4 align="center"><strong>Desktop Design</strong></h4>
+
+<p align="center">
+<img src="./solution-screenshots/desktop.png" alt="desktop page">
+
+<h4 align="center"><strong>Mobile Design</strong></h4>
+
+<p align="center">
+<img src="./solution-screenshots/mobile.png" alt="mobile page">
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Flexbox
+- Mobile-first workflow
+
+### What I learned
+
+Thanks to this project I learned how to use Flexbox. Not only that, this project allowed me to fully understand its key concept.
+
+Furthermore, to make the Website populate content based of the local JSON file data I had to search more about the fetch function. Thanks to this function we can extract data from the JSON file, and based on the data received the Website displays different information.
+
+```js
+fetch("../src/data.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    const averageScoreElement = document.querySelector(
+      ".app__result-circle-total"
+    );
+    averageScoreElement.innerHTML += 0;
+    console.log("error: " + err);
+  });
+```
+
+## Author
+
+- Frontend Mentor - [@Bottom17](https://www.frontendmentor.io/profile/Bottom17)
---
+++ src/utils/insertData.js
@@ -0,0 +1,87 @@
+fetch("../src/data.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    const averageScoreElement = document.querySelector(
+      ".app__result-circle-total"
+    );
+    averageScoreElement.innerHTML += 0;
+    console.log("error: " + err);
+  });
+
+function appendData(data) {
+  var averageScore = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    averageScore += data[i].score;
+
+    switch (data[i].category) {
+      case "reaction":
+        const reactionScoreElement = document.querySelector(
+          ".app__summary-reaction-score1"
+        );
+        const reactionScore = document.createTextNode(data[i].score);
+        reactionScoreElement.appendChild(reactionScore);
+        break;
+      case "memory":
+        const memoryScoreElement = document.querySelector(
+          ".app__summary-memory-score1"
+        );
+        const memoryScore = document.createTextNode(data[i].score);
+        memoryScoreElement.appendChild(memoryScore);
+        break;
+      case "verbal":
+        const verbalScoreElement = document.querySelector(
+          ".app__summary-verbal-score1"
+        );
+        const verbalScore = document.createTextNode(data[i].score);
+        verbalScoreElement.appendChild(verbalScore);
+        break;
+      case "visual":
+        const visualScoreElement = document.querySelector(
+          ".app__summary-visual-score1"
+        );
+        const visualScore = document.createTextNode(data[i].score);
+        visualScoreElement.appendChild(visualScore);
+        break;
+    }
+  }
+  const subtitleElement = document.querySelector(".app__result-subtitle");
+  const descriptionElement = document.querySelector(".app__result-description");
+
+  averageScore /= data.length;
+
+  if (averageScore >= 90) {
+    subtitleElement.innerHTML += "Outstanding";
+    descriptionElement.innerHTML +=
+      "Congratulations! You scored higher than 95% of the people who have taken these tests.";
+  } else if (averageScore >= 70) {
+    subtitleElement.innerHTML += "Superior";
+    descriptionElement.innerHTML +=
+      "Well done! You scored higher than 65% of the people who have taken these tests.";
+  } else if (averageScore >= 50) {
+    subtitleElement.innerHTML += "Satisfactory";
+    descriptionElement.innerHTML += "You passed the test by a thin margin.";
+  } else if (averageScore >= 30) {
+    subtitleElement.innerHTML += "Low Failure";
+    descriptionElement.innerHTML += "You failed!";
+  } else {
+    subtitleElement.innerHTML += "Low Failure";
+    descriptionElement.innerHTML +=
+      "You will have to work a lot harder next time!";
+  }
+
+  const averageScoreElement = document.querySelector(
+    ".app__result-circle-total"
+  );
+
+  if (averageScore === 0 || averageScore === null || averageScore > 100) {
+    averageScoreElement.innerHTML += 0;
+  } else {
+    averageScoreElement.innerHTML += Math.round(averageScore);
+  }
+}
